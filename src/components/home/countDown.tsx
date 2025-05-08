5c9f3255d8b4669156e373318cc310155f87fa79
shivam7703 <shivamg7703@gmail.com>
2025-05-08 12:55:28 +0530

    n

--- src/components/home/countDown.tsx
+++ src/components/home/countDown.tsx
@@ -2,25 +2,32 @@
 import React from 'react';
 import CountUp from 'react-countup';
 
+
+
 function CountDown({ data }: any) {
+ 
+
+
   return (
     <section
       className="bg-gradient1 w-[80vw] lg:px-24 mb-6 sm:mb-14 md:p-16 sm:p-12 p-7 max-w-[1350px] mx-auto rounded-3xl flex flex-wrap gap-6 md:justify-between justify-center"
     >
       {data?.map((counts: any) => (
         <div key={counts.id} className="text-center space-y-2">
-          <h3 className="font-semibold text-xl" style={{ color: counts.textColor }}>
+        <h3 className="font-semibold text-xl" style={{ color: counts.textColor }}>
             {counts.title}
           </h3>
-          <CountUp
-            start={0}
-            end={counts.Value}
-            delay={0.2}
-            duration={2.75}
-            separator=","
-         />
-           
-           <span>{counts.Value2 || "+"}</span>
+            <CountUp
+              start={0}
+              end={counts.Value}
+              delay={0.2}
+              duration={2.75}
+              separator=","
+            />
+                <div className="text-3xl text-black md:text-5xl font-bold">
+                  {counts.value2 || "+"}
+                </div>
+         
         </div>
       ))}
     </section>
