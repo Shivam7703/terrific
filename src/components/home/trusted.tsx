f88e4dd52f6b641eaa9d7a93ba1c76b3c6510f9a
shivam7703 <shivamg7703@gmail.com>
2025-06-16 10:25:55 +0530

    client

--- src/components/home/trusted.tsx
+++ src/components/home/trusted.tsx
@@ -4,26 +4,25 @@ import Image from "next/image";
 
 function Trusted({ data }: any) {
   return (
-    <section className="lg:px-28 sm:p-12 py-7">
-      <h3 className="text-center md:text-2xl font-bold mb-9 text-xl text-zinc-800">
-        Trusted by 40+ clients around the globe
-      </h3>
+    <section className="lg:px-28 sm:p-12 !pb-0 pt-7">
+      <h3 className="text-center md:text-3xl font-bold mb-9 text-xl text-zinc-800">
+We Are Official Channel Partner of      </h3>
 
       {data && data.length > 0 && (
         <Marquee
-          className="w-full h-full"
-          speed={100}
+          className=" h-full"
+          speed={50}
           pauseOnHover={true}
           gradientWidth={100}
           gradient={true}
           gradientColor={"white"} 
         >
           {data.map((bnr: any) => (
-            <div key={bnr.id} className="mx-5">
+            <div key={bnr.id} className="mx-7">
               <Image
                 src={bnr.img}
                 alt={`banner-image-${bnr.id}`}
-                className="w-auto mr-12 h-14 object-contain"
+                className="w-auto  h-14 object-contain"
                 width={208} 
                 height={36} 
               />
